fea2fdc213e534689d53215cb44a29839546615f
George G. Vega Yon <g.vegayon@gmail.com>
2020-06-08 16:39:32 -0700

    Adding plots for dissertation

--- prototype.R
+++ prototype.R
@@ -29,7 +29,7 @@ conditional_dist <- function(model, theta) {
   S. <- ergm::ergm.allstats(model)
   
   seq1 <- sort(unique(S.$statmat[,1]))
-  seq2 <- sort(unique(S.$statmat[,1]))
+  seq2 <- sort(unique(S.$statmat[,2]))
   
   # Computing the conditional probability
   ans <- vector("list", length(seq2))
@@ -44,11 +44,14 @@ conditional_dist <- function(model, theta) {
     # Identifying which are zero
     are0 <- which(a < 0.000000001)
     
-    # if (length(are0)) {
-    #   a <- a[-are0]
-    #   e <- seq1[-are0]
-    # } else
-    #   e <- seq1
+    if (length(are0)) {
+      a <- a[-are0]
+      e <- seq1[-are0]
+    } else
+      e <- seq1
+    
+    if (!length(e))
+      return(c(NA, NA, NA))
     
     e[c(
       # 5% 
@@ -62,14 +65,113 @@ conditional_dist <- function(model, theta) {
   })
   quantiles <- t(quantiles)
   
-  list(
-    x = seq2,
-    y = cbind(
-      `5%`= quantiles[,1],
-      `50%` = quantiles[,2],
-      `95%` = quantiles[,3]
-      )
+  # Which quantile of the conditional is closer to the 50%
+  ergm_probs <- ergmito::exact_loglik(
+    S.$statmat,
+    params        = theta,
+    stats_weights = S.$weights,
+    stats_statmat = S.$statmat,
+    as_prob       = TRUE
+    ) * S.$weights
+  
+  ord <- order(S.$statmat[,2], decreasing = FALSE)
+  ergms_probs <- ergm_probs[ord]
+  
+  b_50pcent <- which.min(abs(cumsum(ergm_probs) - .5))
+  b_50pcent <- S.$statmat[ord,][b_50pcent,2]
+  b_max     <- S.$statmat[ord,][which.max(ergms_probs),2]
+  
+  structure(
+    list(
+      a = seq1,
+      b = seq2,
+      pr = ans,
+      quantiles = cbind(
+        `5%`= quantiles[,1],
+        `50%` = quantiles[,2],
+        `95%` = quantiles[,3]
+        ),
+      par_names = colnames(S.$statmat),
+      probs2    = ergm_probs,
+      b_50pcent = b_50pcent,
+      b_max     = b_max,
+      support   = S.
+      ),
+    class = "ergm_conditional"
   )
+}
+
+namer <- function(x) {
+  switch(
+    x, 
+    edges = "Edge count",
+    ttriple = "Transitive Triads",
+    mutual = "Mutual Ties",
+    nodeicov.gender = "Gender Receiver Effect",
+    nodematch.gender = "Gender Homophily"
+  )
+}
+
+#' Plot method for conditional probabilities on ERGMs
+#' @noRd
+plot.ergm_conditional <- function(x, xlab = namer(x$par_names[2]), ylab = namer(x$par_names[1]), ...) {
+  
+  y_range <- range(x$a)
+  x_range <- range(x$b)
+  
+  # # Border
+  # dat <- data.frame(
+  #   x = c(x$b, rev(x$b)),
+  #   y = c(x$quantile[,1], rev(x$quantile[,3]))
+  # )
+  # 
+  # ggplot2::ggplot(data = dat, aes(x=x,y=y)) +
+  #   ggplot2::geom_polygon()
+  # 
+  graphics::plot(
+    NA,
+    xlim = x_range,
+    ylim = y_range,
+    xlab = xlab,
+    ylab = ylab
+    )
+  
+  op <- graphics::par(xpd = FALSE)
+  graphics::grid()
+  graphics::par(op)
+
+  # Drawing a polygon
+  graphics::polygon(
+    x = c(x$b, rev(x$b)),
+    y = c(x$quantile[,1], rev(x$quantile[,3])),
+    col = "lightgray",
+    border = "darkgray"
+      
+  )
+  
+  lines(
+    y    = x$quantiles[,2],
+    x    = x$b,
+    type = "l", col = "red", lwd = 2, lty = 2
+  )
+  
+  abline(v = x$b_50pcent, lty = 2, lwd = 2)
+  
+  return()
+  
+}
+
+# Plot the first with a title
+plot_first <- function(x, main="", ...) {
+  for (i in seq_along(x)) {
+    plot(x[[i]], ...)
+    if (i == 1L) {
+      op <- par(xpd=NA)
+      title(main = main, line = 1.25, font.main = 1, cex.main = 1.5)
+      par(op)
+    }
+  }
+
 }
 
 # Testing the function
@@ -79,47 +181,74 @@ theta <- c(.5, -.5)
 # Stats counts
 x <- network(matrix(0, 5, 5))
 set.vertex.attribute(x, "gender", c(0,1,0,1,0))
-conditional_dist(x ~ mutual + nodematch("gender"), theta = theta)
 
-# Getting the ranges
-r_edges <- sort(unique(S$statmat[,1]))
-r_ttriad <- range(S$statmat[,2])
 
-r_ttriad <- sort(unique(S$statmat[,2]))
-ans <- vector("list", length(r_ttriad))
-for (i in seq_along(ans)) {
-  ans[[i]] <- d_ergm(r_edges, r_ttriad[i])
-}
+# General plotting parameters
+parpar <- list(
+  mfcol = c(4, 2), mar = c(5,2.5,.5,1.5), oma = c(0,2,2.25,0)
+)
 
-# Plotting:  -------------------------------------------------------------------
 
-# 5%
-x <- sapply(ans, function(a) {
-  
-  a <- cumsum(a)
-  
-  # Identifying which are zero
-  are0 <- which(a < 0.000000001)
-  
-  if (length(are0)) {
-    a <- a[-are0]
-    e <- r_edges[-are0]
-  } else
-    e <- r_edges
+# Mutual --------------------------------------
+models_mutual <- list(
+  x ~ mutual + edges,
+  x ~ mutual + ttriad,
+  x ~ mutual + nodematch("gender"),
+  x ~ mutual + nodeicov("gender")
+)
 
-  e[c(
-    # 5% 
-    which.min(abs(a - .05)),
-    # 50%
-    which.min(abs(a - .5)),
-    # %95
-    which.min(abs(a - .95))
-  )]
+ans_same <- lapply(models_mutual, conditional_dist, theta = c(0, .5))
+ans_diff <- lapply(models_mutual, conditional_dist, theta = c(1, .5))
 
-})
+graphics.off()
+pdf("conditional-prob-mutuals.pdf", width = 5, height = 7)
+op <- do.call(par, parpar)
+plot_first(ans_same, main = "(a)")
+plot_first(ans_diff, main = "(b)")
+par(op)
+title(ylab = "Number of Mutual ties")
+dev.off()
 
-plot(x = r_ttriad, y = x[2,], type = "b", ylim = range(r_edges), lty = 2,
-     xlab = colnames(S$statmat)[2], ylab = colnames(S$statmat)[1])
-lines(x = r_ttriad, y = x[1,], type = "l", col = "red", lwd = 2, lty = 2)
-lines(x = r_ttriad, y = x[3,], type = "l", col = "blue", lwd = 2, lty = 2)
-grid(nx = length(r_ttriad), length(r_edges))
+# Triads --------------------------------------
+models_ttriad <- list(
+  x ~ ttriad + edges,
+  x ~ ttriad + mutual,
+  x ~ ttriad + nodematch("gender"),
+  x ~ ttriad + nodeicov("gender")
+)
+
+ans_same <- lapply(models_ttriad, conditional_dist, theta = c(0, 0))
+ans_diff <- lapply(models_ttriad, conditional_dist, theta = c(1, 0))
+
+graphics.off()
+pdf("conditional-prob-ttriad.pdf", width = 5, height = 7)
+op <- do.call(par, parpar)
+plot_first(ans_same, main = "(a)")
+plot_first(ans_diff, main = "(b)")
+par(op)
+title(ylab = "Number of Transitive Triads")
+dev.off()
+
+# Gender homophily --------------------------------------
+models_homophily <- list(
+  x ~ nodematch("gender") + edges,
+  x ~ nodematch("gender") + mutual,
+  x ~ nodematch("gender") + ttriad,
+  x ~ nodematch("gender") + nodeicov("gender")
+)
+
+ans_same <- lapply(models_homophily, conditional_dist, theta = c(0, 0))
+ans_diff <- lapply(models_homophily, conditional_dist, theta = c(1, 0))
+
+graphics.off()
+pdf("conditional-prob-homophily.pdf", width = 5, height = 7)
+op <- do.call(par, parpar)
+plot_first(ans_same, main = "(a)")
+plot_first(ans_diff, main = "(b)")
+par(op)
+title(ylab = "Number of Homophilic Ties")
+# par(mfrow = c(4, 2), new = FALSE)
+# plot.new()
+# plot.window(c(0,1), c(0,1))
+# title(xlab = "a")
+dev.off()
